ff3d341802700ff24cdb93d30f90b1ade739175c
Akshay Khedkar <2134akshay@gmail.com>
2026-07-08 11:14:38 +0530

    Create Secon.java

---
+++ Secon.java
@@ -0,0 +1,13 @@
+public class ReverseString {
+    public static void main(String[] args) {
+        String str = "Akshay";
+        StringBuilder rev = new StringBuilder();
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev.append(str.charAt(i));
+        }
+
+        System.out.println("Original String: " + str);
+        System.out.println("Reversed String: " + rev.toString());
+    }
+}
