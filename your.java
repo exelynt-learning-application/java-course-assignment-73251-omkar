6a4312af3e1b316f1b65e3577643b8274d4908f3
Akshay Khedkar <2134akshay@gmail.com>
2026-07-08 11:09:18 +0530

    Update your.java

--- your.java
+++ your.java
@@ -1,8 +1,14 @@
 
-StringBuilder rev = new StringBuilder();
+public class ReverseString {
+    public static void main(String[] args) {
+        String str = "Akshay";
+        StringBuilder rev = new StringBuilder();
 
-for (int i = str.length() - 1; i >= 0; i--) {
-    rev.append(str.charAt(i));
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev.append(str.charAt(i));
+        }
+
+        System.out.println("Original String: " + str);
+        System.out.println("Reversed String: " + rev.toString());
+    }
 }
-
-System.out.println(rev.toString());
