a5de0190c2fcf1c985977eecd7817bb0b1270edc
Akshay Khedkar <2134akshay@gmail.com>
2026-07-08 12:14:58 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,13 +1,6 @@
-public class ReverseString {
+public class HelloJava {
     public static void main(String[] args) {
-        String str = "Akshay";
-        StringBuilder rev = new StringBuilder();
-
-        for (int i = str.length() - 1; i >= 0; i--) {
-            rev.append(str.charAt(i));
-        }
-
-        System.out.println("Original String: " + str);
-        System.out.println("Reversed String: " + rev.toString());
+        System.out.println("Khedkar");
     }
 }
+    
